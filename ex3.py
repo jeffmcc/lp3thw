53fc3b0044ef5f2a3a7e5156fca8478012483a2e
jefmc <jeff@blackwing.org>
2018-09-18 22:56:27 -0500

    ex3.py study drill, add comments to each line.

--- ex3.py
+++ ex3.py
@@ -1,23 +1,37 @@
+# Prints a string in quotes ""
 print("I will now count my chickens:")
 
+# Prints the string in quotes "" and the result of the equation. 30.
 print("Hens", 25 + 30 / 6)
+# Prints the string in quotes "" and the result of the equation. 97
 print("Roosters", 100 - 25 * 3 % 4)
 
+# Prints the string in quotes ""
 print("Now I will count the eggs:")
 
+# Prints the result of the equation
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 +6)
 
+# Prints the string in quotes ""
 print("Is it true that 3 + 2 < 5 - 7?")
 
+# Prints the result of the equation
 print(3 + 2 < 5 - 7)
 
+# Prints the string in quotes "" and the result of the equation. 5
 print("What is 3 + 2?", 3 + 2)
+# Prints the string in quotes "" and the result of the equation. -2
 print("What is 5 - 7?", 5 - 7)
 
+# Prints the string in quotes ""
 print("Oh, that's why it's False.")
 
+# Prints the string in quotes ""
 print("How about some more.")
 
+# Prints the string in quotes "" and true/false for the evaluation
 print("Is it greater?", 5 > -2)
+# Prints the string in quotes "" and true/false for the evaluation
 print("Is it greater or equal?", 5 >= -2)
+# Prints the string in quotes "" and true/false for the evaluation
 print("Is it less or equal?", 5 <= -2)
