166659e5b545ba51bd3d61970e1d0ecae9ca298b
jefmc <jeff@blackwing.org>
2018-09-22 22:44:59 -0500

    ex6.py working; now with more Vim.

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # lp3thw
-hundred_days_of_code = 100
-days_completed = 4
-days_remaining = hundred_days_of_code - days_completed
-
+# put something clever here.
 
---
+++ ex6.py
@@ -0,0 +1,42 @@
+import os
+
+print("This is ex6.py freestyle")
+
+print("Took time to switch from Atom back to Vim.") 
+print("Even with Atom's emulation of Vim.")
+print("I think I'm better off with Vim.")
+
+print("What about code completion and tabbed windows?")
+print("What about the time it takes to pimp Vim?")
+
+print("")
+################################################################################
+# begin Excercise 6
+print("\n")
+
+types_of_people = 10
+x = f"There are {types_of_people} types of people."
+
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those that {do_not}."
+
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious))
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print(w + e)
+
+
+
+
